1715d5454e9c9f21602f27f4988d79f4211a53ec
Graham Brereton <graham.brereton@gmail.com>
2015-09-05 16:50:51 -0400

    Add tests for different overflow values in RateProcessor

--- Tests/Tests/Devices/RateProcessorTest.cs
+++ Tests/Tests/Devices/RateProcessorTest.cs
@@ -30,7 +30,7 @@ namespace Tests
 		}
 
 		[Test]
-		public void ProcessEventRateOverflow ()
+		public void ProcessRateEventOverflow ()
 		{
 			var processor = new RateProcessor (65536, 65536);
 
@@ -49,6 +49,50 @@ namespace Tests
 			Assert.AreEqual (2831, processor.EventTime);
 			Assert.AreEqual (2, processor.EventCount);
 			Assert.AreEqual (1.44, Math.Round((double) processor.Rate, 2));
+		}
+
+		[Test]
+		public void ProcessRateEvent65536_256 ()
+		{
+			var processor = new RateProcessor (65536, 256);
+
+			Assert.IsNull (processor.EventTime);
+			Assert.IsNull (processor.EventCount);
+			Assert.IsNull (processor.Rate);
+
+			processor.ProcessRateEvent(65530, 254);
+
+			Assert.AreEqual (65530, processor.EventTime);
+			Assert.AreEqual (254, processor.EventCount);
+			Assert.IsNull (processor.Rate);
+
+			processor.ProcessRateEvent(2831, 2);
+
+			Assert.AreEqual (2831, processor.EventTime);
+			Assert.AreEqual (2, processor.EventCount);
+			Assert.AreEqual (1.44, Math.Round((double) processor.Rate, 2));
+		}
+
+		[Test]
+		public void ProcessRateEvent256_65536 ()
+		{
+			var processor = new RateProcessor (256, 65536);
+
+			Assert.IsNull (processor.EventTime);
+			Assert.IsNull (processor.EventCount);
+			Assert.IsNull (processor.Rate);
+
+			processor.ProcessRateEvent(250, 65534);
+
+			Assert.AreEqual (250, processor.EventTime);
+			Assert.AreEqual (65534, processor.EventCount);
+			Assert.IsNull (processor.Rate);
+
+			processor.ProcessRateEvent(57, 2);
+
+			Assert.AreEqual (57, processor.EventTime);
+			Assert.AreEqual (2, processor.EventCount);
+			Assert.AreEqual (65.02, Math.Round((double) processor.Rate, 2));
 		}
 	}
 }
